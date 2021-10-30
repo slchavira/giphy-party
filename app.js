f7591972c1cf0dadda116a25de6ed447d791064a
Stoner Chavira <slchavira@gmail.com>
2021-10-29 20:40:32 -0500

    Uploading final code for exercise

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-$("form").on("submit", async function(e){
+$("#submit-btn").on("click", async function(e){
     e.preventDefault();
     const query = $("input").val();
+    $("#search").val('');
     const res = await axios.get('http://api.giphy.com/v1/gifs/search', {params: 
     {
         q: query,
         api_key: '7c46GY88xbtmTJCJ19eWTAzYkVA7dYb2'
     }} );
-    console.log(res);
+    // console.log(res);
+    addGif(res.data);
 });
 
+// Create function that takes repsonse, generates a random index, 
+// creates element, and adds to page
+function addGif(data){
+    let idx = Math.floor(Math.random() * data.data.length);
+    let newDiv = $("<div>", { class: "col-md-4 mb-4" });
+    let newGif = $("<img>", { src: data.data[idx].images.original.url, class: "w-100"});
+    newDiv.append(newGif);
+    $("#gifsGoHere").append(newDiv);
+    console.log(data);
+}
 
-
-
-// async function getRandomDog(){
-//     const res = await axios.get('https://dog.ceo/api/breeds/image/random');
-//     console.log(res.data);
-//     const img = document.querySelector("#dog");
-//     img.src = res.data.message;
-// }
-
-// async function getDogByBreed(breed){
-//     try {
-//         const url = `https://dog.ceo/api/breed/${breed}/images/random`
-//         const res = await axios.get(url);
-//         const img = document.querySelector("#dog");
-//         img.src = res.data.message;
-//     } catch (e) {
-//         alert("Breed Not Found")
-//         getRandomDog();
-//     }
-// }
-
-// const form = document.querySelector("#searchForm");
-// const input = document.querySelector("#search");
-// form.addEventListener("submit", function(e){
-//     e.preventDefault();
-//     getDogByBreed(input.value);
-//     input.value = '';
-// })
+// add event listener to remove all images from page
+$("#remove").on("click", function(e){
+    console.log(e.target.id);
+    if (e.target.id === 'remove') {
+        $("#gifsGoHere").empty();
+        console.log("Emptied");
+    }
+});
